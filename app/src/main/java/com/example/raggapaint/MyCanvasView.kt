18c86269636eab880dd6fe223f8f0dab2e0e5aa9
Jens Krause <jens@rasta.online>
2020-05-12 15:04:07 +0700

    implement onSizeChanged: create new Canvas from Bitmap of the correct size

--- app/src/main/java/com/example/raggapaint/MyCanvasView.kt
+++ app/src/main/java/com/example/raggapaint/MyCanvasView.kt
@@ -1,7 +1,28 @@
 package com.example.raggapaint
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 
 class MyCanvasView(context: Context): View(context) {
+
+    //for caching what has been drawn before
+    private lateinit var extraCanvas:Canvas
+    private lateinit var extraBitmap: Bitmap
+
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    override fun onSizeChanged(width: Int, height: Int, oldwidth: Int, oldheight: Int) {
+        super.onSizeChanged(width, height, oldwidth, oldheight)
+        // to prevent memory leak on creating new Bitmaps, recycle an existing
+        if(::extraBitmap.isInitialized) extraBitmap.recycle()
+        // create new Bitmap with new height/width and the recommended color profile
+        extraBitmap = Bitmap.createBitmap(width,height,Bitmap.Config.ARGB_8888)
+        // create new Canvas for this Bitmap
+        extraCanvas = Canvas(extraBitmap)
+        // draw the background color
+        extraCanvas.drawColor(backgroundColor)
+    }
 }
